5090ed9b08a6dd9dceabb1a1f219b9bc5066d002
unknown <F219425@NUCFD.EDU.LOCAL>
2023-10-06 10:12:40 +0500

    Created a calculator

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+package newCalc;
+
+public class Calculator{
+	private int var1;
+	private int var2;
+	
+	public Calculator(){
+		
+	}
+	
+	public void add(int v1 , int v2){
+		System.out.println(v1+v2);
+	}
+	
+	public void sub(int v1, int v2)
+	{
+		System.out.println(v1-v2);
+	}
+	
+	
+	public static void main(String[] args){
+		
+		Calculator c = new Calculator();
+		
+		c.add(1,2);
+		c.sub(2,3);
+		
+	}
+	
+	
+}
